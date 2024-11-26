e63e2eeac2aa9a2e33fac37e46a7c42d5fea3e0a
Thaina <thainalopes@gmail.com>
2024-11-26 11:40:25 -0300

    Centraliza o título

--- src/componentes/header.js
+++ src/componentes/header.js
@@ -5,16 +5,10 @@ import styles from "../componentes/header.module.css";
 export default function Header() {
   return (
     <header className={styles.headerContainer}>
-      <nav className={styles.menuContainer}>
+      <div className={styles.menuContainer}>
         <div className={styles.menuItem}>
           <a href="/perfil" aria-label="Ir para o perfil">
-            <RxAvatar
-              size={28}
-              aria-hidden="true"
-              style={{
-                filter: "drop-shadow(2px 2px 5px rgba(255, 255, 255, 1))",
-              }}
-            />
+            <RxAvatar size={30} aria-hidden="true" />
           </a>
         </div>
         <div className={styles.menuItem}>
@@ -24,7 +18,7 @@ export default function Header() {
           </div>
         </div>
         <div className={styles.menuItem}></div>
-      </nav>
+      </div>
     </header>
   );
 }
